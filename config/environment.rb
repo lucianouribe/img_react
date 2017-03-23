cf15f536669e66c5f7a91ffcf24e51e4c293e304
Luciano Uribe <lucianouribe@gmail.com>
2017-03-23 12:19:26 -0400

    fix devise

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# config.gem "ambethia-smtp-tls", :lib => "smtp-tls", :source => "http://gems.github.com/"

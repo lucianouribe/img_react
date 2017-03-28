65182ebb2dbcb79e9f78d96352f0d86587873e6e
Luciano Uribe <lucianouribe@gmail.com>
2017-03-28 18:14:25 -0400

    apllication mime

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,8 @@ module ReactWebsite
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.action_dispatch.default_headers = {
+      'X-Frame-Options' => 'DENY',
+    }
   end
 end

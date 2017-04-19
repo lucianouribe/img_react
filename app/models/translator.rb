fde34212d5e6eaec0d02fce0a7ef01cb57136d82
Luciano Uribe <lucianouribe@gmail.com>
2017-04-19 15:01:28 -0500

    translator russian and build
    :q
    
    dert
    
    :q
    
    i
    :i
    sadfgh
    :q

--- app/models/translator.rb
+++ app/models/translator.rb
@@ -36,7 +36,7 @@ class Translator < ApplicationRecord
         end
       end
 
-      info[:lang_second] = 'cirilic'
+      # info[:lang_second] = 'cirilic'
       info[:outphrase] = @text_in_array.join
       info
     elsif info[:lang_first] == 'english'
@@ -73,7 +73,7 @@ class Translator < ApplicationRecord
         end
       end
 
-      info[:lang_second] = 'cirilic'
+      # info[:lang_second] = 'cirilic'
       info[:outphrase] = @text_in_array.join
       info
     elsif info[:lang_first] == 'deutsch'
@@ -110,11 +110,86 @@ class Translator < ApplicationRecord
         end
       end
 
-      info[:lang_second] = 'cirilic'
+      # info[:lang_second] = 'cirilic'
       info[:outphrase] = @text_in_array.join
       info
-    end
+    elsif info[:lang_first] == 'ruso'
+      @cirilico = ['a',  'b',  'v',  'h',  'g',  'd', 'e',  'ye', 'zh', 'z',  'y',  'i', 'yi', 'y',  'k',  'l',  'm',  'n', 'o',  'p',  'r',  's',  't',  'u', 'f',  'kh', 'ts', 'ch', 'sh', 'shch', 'yu', 'ya', '', 'i']
+      @latino = ['а',  'б',  'в',  'г',  'г',  'д', 'е',  'є', 'ж', 'з',  'и',  'і', 'ї', 'й',  'к',  'л',  'м',  'н', 'о',  'п',  'р',  'с',  'т',  'у', 'ф',  'х', 'ц', 'ч', 'ш', 'щ',
+      'ю', 'я', 'ь', 'ы']
 
+      # language exceptions
+      # @exception = ['zz', 'll', 'tz', 'sch', 'ie', 'ei', 'ha', 'hei', 'ch', 'sh', 'ä', 'ö', 'ü', 'ß', 'je', 'ehe']
+      # @kirilik = ['ц', 'л', 'ц', 'ш', 'и', 'ай', 'ха', 'хай', 'х', 'ш', 'э', 'оь', 'уй', 'з', 'йе', 'ехе']
+
+
+      # input
+      @text = info[:inphrase].downcase
+
+      # create a rejex for exceptions
+      # @text = user_input.split(/(zz|ll|tz|sch|ie|ei|ha|hei|ch|sh|ä|ö|ü|ß|je|ehe)/)
+      # # exceptor
+      # @text.map.with_index do |letter, num|
+      #   @exception.map.with_index do |font, index|
+      #     if letter == "#{font}"
+      #       @text[num] = @kirilik[index]
+      #     end
+      #   end
+      # end
+
+      # @text = @text.join
+      # separa las letras de la frase
+      @text_in_array = @text.split(%r{\d*})
+
+      # transposer
+      @text_in_array.map.with_index do |letter, num|
+        @latino.map.with_index do |font, index|
+          if letter == "#{font}"
+            @text_in_array[num] = @cirilico[index]
+          end
+        end
+      end
+
+      info[:lang_second] = 'latino'
+      info[:outphrase] = @text_in_array.join
+      info
+
+    end
+  #     string = info[:inphrase]
+  #     binding.pry
+  #     alphabet = {
+  #       'а' => 'a',  'б' => 'b',  'в' => 'v',  'г' => 'h',  'ґ' => 'g',  'д' => 'd',
+  #       'е' => 'e',  'є' => 'ye', 'ж' => 'zh', 'з' => 'z',  'и' => 'y',  'і' => 'i',
+  #       'ї' => 'yi', 'й' => 'y',  'к' => 'k',  'л' => 'l',  'м' => 'm',  'н' => 'n',
+  #       'о' => 'o',  'п' => 'p',  'р' => 'r',  'с' => 's',  'т' => 't',  'у' => 'u',
+  #       'ф' => 'f',  'х' => 'kh', 'ц' => 'ts', 'ч' => 'ch', 'ш' => 'sh', 'щ' => 'shch',
+  #       'ю' => 'yu', 'я' => 'ya',
+  #       'А' => 'a',  'Б' => 'b',  'В' => 'v',  'Г' => 'h',  'Ґ' => 'g',  'Д' => 'd',
+  #       'Е' => 'e',  'Є' => 'ye', 'Ж' => 'zh', 'З' => 'z',  'И' => 'y',  'І' => 'i',
+  #       'Ї' => 'yi', 'Й' => 'y',  'К' => 'k',  'Л' => 'l',  'М' => 'm',  'Н' => 'n',
+  #       'О' => 'o',  'П' => 'p',  'Р' => 'r',  'С' => 's',  'Т' => 't',  'У' => 'u',
+  #       'Ф' => 'f',  'Х' => 'kh', 'Ц' => 'ts', 'Ч' => 'ch', 'Ш' => 'sh', 'Щ' => 'shch',
+  #       'Ю' => 'yu', 'Я' => 'ya',
+  #       'ь' => '' # Ignore symbol, because its standard presentation is not allowed in URLs
+  #     }
+  #     result = string.gsub(/[а-яА-ЯіїєґІЇЄҐ]/, alphabet)
+  #     binding.pry
+  #     result.gsub(/[äöüß]/i) do |match|
+  #       case match.downcase
+  #         when "ä" 'ae'
+  #         when "ö" 'oe'
+  #         when "ü" 'ue'
+  #         when "ß" 'ss'
+  #       end
+  #     end
+  #     binding.pry
+  #
+  #     info[:outphrase] = result.gsub(/\W/, '').downcase
+  #     info
+  #
+  #
+  #   end
+  #
   end
 
 end
--- client/components/Translator.js
+++ client/components/Translator.js
@@ -7,14 +7,15 @@ class Translator extends React.Component {
 
     this.state = {
       transInfo: '',
-      lengua1: 'espanol',
-      lengua2: ''
+      lengua1: 'esp',
+      lengua2: 'cir'
     }
 
     this.answer = this.answer.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.putPlease = this.putPlease.bind(this);
     this.language = this.language.bind(this);
+    this.language2 = this.language2.bind(this);
   }
 
   componentDidMount(){
@@ -22,10 +23,26 @@ class Translator extends React.Component {
       $('.btn-lang').removeClass('clicked');
       $(this).addClass('clicked');
     });
+    $('.btn-lang2').on('click', function(){
+      $('.btn-lang2').removeClass('clicked');
+      $(this).addClass('clicked');
+    });
+    $('.lat1').on('click', function(){
+      $('.lat2').hide();
+      $('.cir2').show();
+    })
+    $('.cir1').on('click', function(){
+      $('.cir2').hide();
+      $('.lat2').show();
+    })
   }
 
   language(idiom) {
     this.setState({ lengua1: idiom })
+  }
+
+  language2(idiom) {
+    this.setState({ lengua2: idiom })
   }
 
   putPlease() {
@@ -55,6 +72,7 @@ class Translator extends React.Component {
     e.preventDefault();
     let inphrase = this.refs.translateMe.value;
     let outphrase;
+
     let lang_first;
     switch (this.state.lengua1) {
       case 'esp':
@@ -66,42 +84,63 @@ class Translator extends React.Component {
       case 'deu':
         lang_first = 'deutsch';
         break;
+      case 'rus':
+        lang_first = 'ruso';
+        break;
       default:
         lang_first = 'espanol';
         break;
     }
-    let lang_second = this.state.lengua2;
-      $.ajax({
-        url: '/api/translator',
-        type: 'POST',
-        dataType: 'JSON',
-        data: { translator: {inphrase, outphrase, lang_first, lang_second }}
-      }).done( data => {
-        // console.log("its hitting done");
-        // console.log(data);
-        this.answer();
-        this.refs.translateForm.reset();
-      }).fail( data => {
-        // console.log("its hitting fail first part");
-        // console.log(data);
-        this.refs.translateForm.reset();
+
+    let lang_second;
+    switch (this.state.lengua2) {
+      case 'cir':
+        lang_second = 'cirilico';
+        break;
+      case 'lat':
+        lang_second = 'latino';
+        break;
+      default:
+        lang_second = 'cirilico';
+        break;
+    }
+
+    $.ajax({
+      url: '/api/translator',
+      type: 'POST',
+      dataType: 'JSON',
+      data: { translator: {inphrase, outphrase, lang_first, lang_second }}
+    }).done( data => {
+      // console.log("its hitting done");
+      // console.log(data);
+      this.answer();
+      this.refs.translateForm.reset();
+    }).fail( data => {
+      // console.log("its hitting fail first part");
+      // console.log(data);
+      this.refs.translateForm.reset();
     })
   }
 
-  // <div type="submit" className="btn btn-translate rus" onClick={() => this.language(russkiy)}>russkiy</div>
   render() {
     let espanol = 'esp';
     let english = 'eng';
     let deutsch = 'deu';
+    let ruso = 'rus';
+    let cirilico = 'cir';
+    let latino = 'lat';
     return (
       <div className="card translate">
         <form ref="translateForm" className="input-field">
           <span className="center title"><h5>Kirilik Translator</h5></span>
-          <div type="button" className="btn btn-lang esp" onClick={() => this.language(espanol)}>español</div>
-          <div type="button" className="btn btn-lang eng" onClick={() => this.language(english)}>english</div>
-          <div type="button" className="btn btn-lang deu" onClick={() => this.language(deutsch)}>deutsch</div>
-          <div type="submit" className="btn btn-translate right" onClick={this.handleSubmit}>translate</div>
+          <div type="button" className="btn btn-lang lat1" onClick={() => this.language(espanol)}>español</div>
+          <div type="button" className="btn btn-lang lat1" onClick={() => this.language(english)}>english</div>
+          <div type="button" className="btn btn-lang lat1" onClick={() => this.language(deutsch)}>deutsch</div>
+          <div type="button" className="btn btn-lang cir1" onClick={() => this.language(ruso)}>ruso</div>
           <textarea className="input" type="text" ref="translateMe" placeholder="Write something"></textarea>
+          <div type="button" className="btn btn-lang2 cir2" onClick={() => this.language2(cirilico)}>cirilico</div>
+          <div type="button" className="btn btn-lang2 lat2" onClick={() => this.language2(latino)}>latino</div>
+          <div type="submit" className="btn btn-translate right" onClick={this.handleSubmit}>translate</div>
         </form>
         <div className="answer">
           {this.putPlease()}

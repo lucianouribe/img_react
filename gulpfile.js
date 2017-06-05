c26497b2e81fd084d6db86f14d3f4b3cc8d83c43
Luciano Uribe <lucianouribe@gmail.com>
2017-06-05 18:56:55 -0500

    gulp style equilibrio

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var autoprefixer = require('gulp-autoprefixer');
 
 // convierte el css en css con todas las excepciones
 gulp.task('styles', function(){
-  gulp.src('./app/assets/stylesheets/calculator.scss').pipe(autoprefixer()).pipe(gulp.dest('gulpStyles'))
+  gulp.src('./app/assets/stylesheets/equilibrio.scss').pipe(autoprefixer()).pipe(gulp.dest('gulpStyles'))
 });
 
 // lo vuelve un task cada cierto tiempo

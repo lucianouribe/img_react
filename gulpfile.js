794c84158cda2ddae25f9206416ef9af1582afed
Luciano Uribe <lucianouribe@gmail.com>
2017-07-09 18:39:53 -0500

    fix home images

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,10 @@ var autoprefixer = require('gulp-autoprefixer');
 
 // convierte el css en css con todas las excepciones
 gulp.task('styles', function(){
-  gulp.src('./app/assets/stylesheets/equilibrio.scss').pipe(autoprefixer()).pipe(gulp.dest('gulpStyles'))
+  gulp.src('./app/assets/stylesheetsA/*.scss').pipe(autoprefixer()).pipe(gulp.dest('./app/assets/stylesheets'))
 });
 
-// lo vuelve un task cada cierto tiempo
+// automaticamente llama al task styles cada que se cambia algo en StylesheetsA
 gulp.task('watch', function(){
-  gulp.watch('./app/assets/stylesheets/calculator.scss', ['styles']);
+  gulp.watch('./app/assets/stylesheetsA/*.scss', ['styles']);
 });

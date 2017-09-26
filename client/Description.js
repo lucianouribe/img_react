02de628b4775ffaf4a7b5eadd7a33cd8e5fbed27
Luciano Uribe <lucianouribe@gmail.com>
2017-09-26 16:39:47 +0200

    descripcion panoramicos

--- client/Description.js
+++ client/Description.js
@@ -2,20 +2,20 @@ module.exports = {
 
   panoramicos: {
     espanol: {
-      name: "Panoramicos",
-      description: "Toda esta belleza son fotos que he tomado en bla blabla, Los renders de productos son imágenes ideales para visualizar un objeto que aún no existe o para diseñar reformas, texturas y decoración a uno existente.\nEstas gráficas son realizadas mediante técnicas de fotorealismo utlizando diferentes softwares de modelación 3D, renderizado y post producción."
+      name: "Panorámicos 360",
+      description: "Actividad fotográfica que exige dedicación, cuidado por los colores, luces y sombras, amor por el detalle y mucha post producción, los panorámicos 360 son imágenes que reproducen un ambiente por sus cuatro costados incluyendo nadir y cenit.\nSon imágenes ideales para mostrar, a través de medios digitales, espacios arquitectónicos, diseños de interior, futuros proyectos o lugares turísticos.\nLos panorámicos 360 pueden ser fotografía directa, un ambiente creado digitalmente o una combinación de ambos."
     },
     ingles: {
-      name: "Panoramics",
-      description: "All this beauty is for bla blabla, Los renders de productos son imágenes ideales para visualizar un objeto que aún no existe o para diseñar reformas, texturas y decoración a uno existente.\nEstas gráficas son realizadas mediante técnicas de fotorealismo utlizando diferentes softwares de modelación 3D, renderizado y post producción."
+      name: "360 Panoramas",
+      description: "It's a photographic activity that demands dedication, care for colors, lights and shadows, love for detail and lots of post production edition, the 360 panoramas are images that reproduce a space by its four sides including the nadir and zenith.\nThis are ideal images to show through digital media architectonic spaces, interior design, future projects or touristic places.\nThe 360 panoramas can ba a direct photography, a digitally modeled space or a mix of both."
     },
     frances: {
-      name: "Panoraiques",
-      description: "Toute cette beaute est pour bla blabla, Los renders de productos son imágenes ideales para visualizar un objeto que aún no existe o para diseñar reformas, texturas y decoración a uno existente.\nEstas gráficas son realizadas mediante técnicas de fotorealismo utlizando diferentes softwares de modelación 3D, renderizado y post producción."
+      name: "Panoramiques 360",
+      description: "C'est un'activité photographique qui exige du dévouement, des soins pour les couleurs, les lumières et les ombres, l'amour pour les détails et beaucoup de post-production, panoramiques 360 sont des images qui reproduisent un environnement sur ses quatre côtés, y compris le nadir et le zénith.\nCe sont des images idéales pour montrer, à travers des médias numériques, des espaces architecturaux, des dessins d'intérieur, des projets futurs ou des lieux touristiques.\nLes panoramiques 360 peuvent être une photographie directe, un environnement modelée numériquement ou une combinaison des deux."
     },
     aleman: {
-      name: "Aussiehen",
-      description: "Die ganze schoneheit ist fur bla blabla, Los renders de productos son imágenes ideales para visualizar un objeto que aún no existe o para diseñar reformas, texturas y decoración a uno existente.\nEstas gráficas son realizadas mediante técnicas de fotorealismo utlizando diferentes softwares de modelación 3D, renderizado y post producción."
+      name: "360 Panoramisch",
+      description: "Entschuldigung! In weniger zeit diese teil wird übersetzt sein."
     },
   },
   renders: {

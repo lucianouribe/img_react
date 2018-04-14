5661b55c7d8d142428f8f18e04a93d5804b48cb9
Luciano Uribe <lucianouribe@gmail.com>
2018-04-14 17:02:04 +0200

    change of topic names and style names

--- client/Tutorials.js
+++ client/Tutorials.js
@@ -1,8 +1,8 @@
 module.exports = {
-  topic: ['select', 'web', 'javascript', 'ruby', 'adobe', 'git', 'server', 'sql', 'tridimax', 'zbrush', 'literatura', 'gastronomy', 'otros'],
+  topic: ['select', 'programming', 'javascript', 'ruby', 'adobe', 'git', 'server', 'sql', 'tridimax', 'zbrush', 'literature', 'gastronomy', 'ideas', 'shopping','others'],
   select: ['none'], // just for having empty options
-  web: ['html', 'css'],
-  javascript: ['vanella', 'es6', 'react', 'redux', 'json'],
+  programming: ['html', 'css', 'command_line', 'atom'],
+  javascript: ['vanella', 'es6', 'react', 'redux', 'json', 'ajax'],
   ruby: ['ruby', 'rails', 'active record', 'json'],
   adobe: ['photoshop', 'illustrator', 'indesign', 'afterfx', 'final_cut'],
   git: ['git', 'github'],
@@ -10,8 +10,10 @@ module.exports = {
   sql: ['postgresql', 'sql'],
   tridimax: ['modeling', 'mental', 'vray', 'texturing', 'animation', 'lighting', 'rigging', 'post_production'],
   zbrush: ['modeling', 'texturing', 'lighting', 'post_production'],
-  literatura: ['gramatica', 'estilo', 'tecnicas_narrativas', 'ideas_libros'],
+  literature: ['gramatica', 'estilo', 'tecnicas_narrativas', 'ideas_libros'],
   gastronomy: ['drinks', 'food', 'desert'],
-  otros: ['architecture', 'arts', 'collumino'],
+  ideas: ['literature', 'disenos', 'programming', 'gastronomy', '3d'],
+  shopping: ['market', 'material', 'gadjets', 'garbage'],
+  others: ['architecture', 'arts', 'collumino', 'education','todo'],
   difficulty: ['basic', 'medium', 'advanced', 'sayayin']
 }

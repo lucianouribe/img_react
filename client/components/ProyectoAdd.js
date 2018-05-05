3ad6fc2d01493a7ee93acf456e9b47f2a295956a
Luciano Uribe <lucianouribe@gmail.com>
2018-05-05 23:13:10 +0200

    proyectos add component

---
+++ client/components/ProyectoAdd.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { addProyecto } from '../actions/proyectos';
+import { ortografica } from '../helpers';
+import Tutorials from '../Tutorials';
+
+class ProyectoAdd extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      cualTopic: 'none',
+      cualSubTopic: ['none']
+    }
+
+    this.setTopics = this.setTopics.bind(this);
+    this.addForm = this.addForm.bind(this);
+    this.addHandleSubmit = this.addHandleSubmit.bind(this);
+  }
+
+  setTopics(aTopic){
+    let picked = Tutorials[aTopic];
+    this.setState({cualTopic: aTopic, cualSubTopic: picked});
+  }
+
+  addHandleSubmit(e){
+    e.preventDefault();
+    // console.log('handle sumbit')
+    let name = this.refs.name.value.toLowerCase();
+    let topic = this.refs.topic.value;
+    let subtopic = this.refs.subtopic.value;
+    let difficulty = this.refs.difficulty.value;
+    let orden = 0;
+    this.props.dispatch(addProyecto(name, topic, subtopic, difficulty, orden, this.props.user.id));
+    this.props.toggleDisplay();
+  }
+
+  addForm(){
+    let elProyecto = Tutorials.topic;
+    let proyectoTopic = elProyecto.map((elPro, i) => {
+      return (
+        <option type="text" key={i} value={elPro} >{elPro}</option>
+      );
+    });
+
+    let elSubProyecto = this.state.cualSubTopic;
+    let subProyectoTopic;
+    if(elSubProyecto !== []) {
+      subProyectoTopic = elSubProyecto.map((subPro, i) => {
+        return (
+          <option type="text" key={i} value={subPro} >{ortografica(subPro)}</option>
+        );
+      });
+    }
+    // select topics could be a component or a separated method?
+    return(
+        <div className="tarjeta form-edit">
+          <form className="input-field" encType="multipart/form-data">
+            <div className="tarjeta-content">
+              <h3 className='tarjeta-title center'>Proyecto nuevo</h3>
+                <input type="text" ref='name' required placeholder="nombre"/>
+                <select className="browser-default" ref="topic" onChange={() => this.setTopics(this.refs.topic.value)}>
+                  {proyectoTopic}
+                </select>
+                <select className="browser-default" ref="subtopic">
+                  {subProyectoTopic}
+                </select>
+                <select className="browser-default" ref="difficulty">
+                  <option>basic</option>
+                  <option>medium</option>
+                  <option>advanced</option>
+                  <option>sayayin</option>
+                </select>
+            </div>
+            <div className="tarjeta-action">
+              <span onClick={this.addHandleSubmit}><i className="material-icons">done</i></span>
+              <span onClick={this.toggleDisplay}><i className="material-icons">cancel</i></span>
+            </div>
+          </form>
+        </div>
+    );
+  }
+
+  render() {
+    return (
+      <div>
+      	 {this.addForm()}
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    user: state.user
+ }
+}
+
+export default connect(mapStateToProps)(ProyectoAdd);
--- client/components/Proyectos.js
+++ client/components/Proyectos.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchProyectos, addProyecto } from '../actions/proyectos';
+import { fetchProyectos } from '../actions/proyectos';
 import { addMemory } from '../actions/mymemory';
-import { ortografica } from '../helpers';
 import Proyecto from './Proyecto';
+import ProyectoAdd from './ProyectoAdd';
 import ProyectosSearch from './ProyectosSearch';
-import Tutorials from '../Tutorials';
 
 class Proyectos extends React.Component {
 
@@ -14,26 +13,17 @@ class Proyectos extends React.Component {
 
     this.state = {
       showForm: false,
-      cualTopic: 'none',
-      cualSubTopic: ['none'],
-      modalize: false
+      modalize: true
     }
 
     this.toggleDisplay = this.toggleDisplay.bind(this);
     this.displayChanger = this.displayChanger.bind(this);
-    this.addForm = this.addForm.bind(this);
-    this.addHandleSubmit = this.addHandleSubmit.bind(this);
-    this.setTopics = this.setTopics.bind(this);
   }
 
   componentDidMount() {
     $('select').material_select();
     let full = 'full'
     this.props.dispatch(fetchProyectos(full));
-    // option for cellphones
-    // let isMobile = (window.innerWidth <= 800 && window.innerHeight <= 600)
-    let isMobile = true
-    this.modalizeMe(isMobile);
   }
 
   modalizeMe(doIt){
@@ -48,76 +38,10 @@ class Proyectos extends React.Component {
   // DISPLAY CHANGER
   displayChanger(){
     if(this.state.showForm) {
-      return(this.addForm());
+      return(<ProyectoAdd toggleDisplay={this.toggleDisplay} />);
     } else {
       return(this.displayProyectos());
     }
-  }
-
-  //SET TOPIC STATER
-  setTopics(aTopic){
-    let picked = Tutorials[aTopic];
-    this.setState({cualTopic: aTopic, cualSubTopic: picked});
-  }
-
-  // ADD FORM
-  addForm(){
-    let elProyecto = Tutorials.topic;
-    let proyectoTopic = elProyecto.map((elPro, i) => {
-      return (
-        <option type="text" key={i} value={elPro} >{elPro}</option>
-      );
-    });
-
-    let elSubProyecto = this.state.cualSubTopic;
-    let subProyectoTopic;
-    if(elSubProyecto !== []) {
-      subProyectoTopic = elSubProyecto.map((subPro, i) => {
-        return (
-          <option type="text" key={i} value={subPro} >{ortografica(subPro)}</option>
-        );
-      });
-    }
-    // select topics could be a component or a separated method?
-    return(
-        <div className="tarjeta form-edit">
-          <form className="input-field" encType="multipart/form-data">
-            <div className="tarjeta-content">
-              <h3 className='tarjeta-title center'>Proyecto nuevo</h3>
-                <input type="text" ref='name' required placeholder="nombre"/>
-                <select className="browser-default" ref="topic" onChange={() => this.setTopics(this.refs.topic.value)}>
-                  {proyectoTopic}
-                </select>
-                <select className="browser-default" ref="subtopic">
-                  {subProyectoTopic}
-                </select>
-                <select className="browser-default" ref="difficulty">
-                  <option>basic</option>
-                  <option>medium</option>
-                  <option>advanced</option>
-                  <option>sayayin</option>
-                </select>
-            </div>
-            <div className="tarjeta-action">
-              <span onClick={this.addHandleSubmit}><i className="material-icons">done</i></span>
-              <span onClick={this.toggleDisplay}><i className="material-icons">cancel</i></span>
-            </div>
-          </form>
-        </div>
-    );
-  }
-
-  // ADD FORM SUBMIT
-  addHandleSubmit(e){
-    e.preventDefault();
-    // console.log('handle sumbit')
-    let name = this.refs.name.value.toLowerCase();
-    let topic = this.refs.topic.value;
-    let subtopic = this.refs.subtopic.value;
-    let difficulty = this.refs.difficulty.value;
-    let orden = 0;
-    this.props.dispatch(addProyecto(name, topic, subtopic, difficulty, orden, this.props.user.id));
-    this.toggleDisplay();
   }
 
   // DISPLAY PROYECTOS
@@ -187,7 +111,6 @@ class Proyectos extends React.Component {
 
 const mapStateToProps = (state) => {
   return {
-    user: state.user,
     proyectos: state.proyectos,
     mymemory: state.mymemory
  }

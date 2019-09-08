980e9b2598f707beb9be41c263e42e2d6a3ddf06
Luciano Uribe <lucianouribe@gmail.com>
2019-09-08 16:23:14 +0200

    added ituto to active admin

---
+++ app/admin/proyectos.rb
@@ -0,0 +1,15 @@
+ActiveAdmin.register Proyecto do
+# See permitted parameters documentation:
+# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+#
+permit_params :name, :topic, :subtopic, :difficulty, :orden 
+#
+# or
+#
+# permit_params do
+#   permitted = [:permitted, :attributes]
+#   permitted << :other if params[:action] == 'create' && current_user.admin?
+#   permitted
+# end
+
+end

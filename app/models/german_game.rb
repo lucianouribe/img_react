cb41a46c8bfd63f9bdcc5e030ef4917687b8de9d
Luciano Uribe <lucianouribe@gmail.com>
2019-09-30 19:38:18 +0200

    validations

--- app/models/german_game.rb
+++ app/models/german_game.rb
@@ -1,3 +1,6 @@
 class GermanGame < ApplicationRecord
   belongs_to :user
+
+  validates :user_id, uniqueness: true
+  validates :lifes, :inclusion => 0..3 
 end
--- db/migrate/20190930160501_create_german_games.rb
+++ db/migrate/20190930160501_create_german_games.rb
@@ -1,10 +1,10 @@
 class CreateGermanGames < ActiveRecord::Migration[5.1]
   def change
     create_table :german_games do |t|
-      t.integer :lifes
-      t.integer :punctuation
-      t.integer :punctuation_4_total
-      t.integer :level
+      t.integer :lifes, default: 3
+      t.integer :punctuation, default: 0
+      t.integer :punctuation_4_total, default: 0
+      t.integer :level, default: 1
       t.belongs_to :user, foreign_key: true
 
       t.timestamps

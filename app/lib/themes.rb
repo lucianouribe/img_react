77098c7a41f0c06af8f5b57e07c3f468f2deb437
Luciano Uribe <lucianouribe@gmail.com>
2019-10-04 20:01:54 +0200

    add all subthemes

--- app/lib/themes.rb
+++ app/lib/themes.rb
@@ -7,16 +7,16 @@ module Themes
       'essen' => 'essen',
       'einkaufen' => 'einkaufen',
       'kleidung' => 'kleidung',
-      # 'zuhause' => 'zuhause',
-      # 'draussen' => 'draussen',
-      # 'tiere' => 'tiere',
-      # 'machen' => 'machen',
-      # 'arbeit' => 'arbeit',
-      # 'verkehr' => 'verkehr',
-      # 'wetter' => 'wetter',
-      # 'zeit' => 'zeit',
-      # 'welt' => 'welt',
-      # 'math' => 'math',
+      'zuhause' => 'zuhause',
+      'draussen' => 'draussen',
+      'tiere' => 'tiere',
+      'machen' => 'machen',
+      'arbeit' => 'arbeit',
+      'verkehr' => 'verkehr',
+      'wetter' => 'wetter',
+      'zeit' => 'zeit',
+      'welt' => 'welt',
+      'math' => 'math',
     }
   end
 
@@ -25,6 +25,16 @@ module Themes
             .merge(essen)
             .merge(einkaufen)
             .merge(kleidung)
+            .merge(zuhause)
+            .merge(draussen)
+            .merge(tiere)
+            .merge(machen)
+            .merge(arbeit)
+            .merge(verkehr)
+            .merge(wetter)
+            .merge(zeit)
+            .merge(welt)
+            .merge(math)
   end
 
   def self.subtheme(option = 'familie')
@@ -80,6 +90,117 @@ module Themes
       'ziehen' => 'ziehen',
       'kapuze' => 'kapuze',
       'hose' => 'hose',
+    }
+  end
+
+  def self.zuhause
+    {
+      '-- ZUHAUSE --' => '',
+      'zimmer' => 'zimmer',
+      'haus' => 'haus',
+      'kueche' => 'kueche',
+      'wohnzimmer' => 'wohnzimmer',
+      'gebaut' => 'gebaut',
+    }
+  end
+
+  def self.draussen
+    {
+      '-- DRAUSSEN --' => '',
+      'spielplatz' => 'spielplatz',
+      'stadt' => 'stadt',
+      'garten' => 'garten',
+      'park' => 'park',
+      'wald' => 'wald',
+      'bauernhof' => 'bauernhof',
+      'wasser' => 'wasser',
+      'bergen' => 'bergen',
+    }
+  end
+
+  def self.tiere
+    {
+      '-- TIERE --' => '',
+      'haustiere' => 'haustiere',
+      'waldtiere' => 'waldtiere',
+      'bauerhoftiere' => 'bauerhoftiere',
+      'fische' => 'fische',
+      'vogel' => 'vogel',
+      'zootiere' => 'zootiere',
+    }
+  end
+
+  def self.machen
+    {
+      '-- MACHE --' => '',
+      'spielen' => 'spielen',
+      'malen' => 'malen',
+      'sport' => 'sport',
+      'bucher' => 'bucher',
+      'urlaub' => 'urlaub',
+      'ausflug' => 'ausflug',
+      'museum' => 'museum',
+      'experiment' => 'experiment',
+      'marchen' => 'marchen',
+    }
+  end
+
+  def self.arbeit
+    {
+      '-- ARBEIT --' => '',
+      'schule' => 'schule',
+      'polizei' => 'polizei',
+      'feuerwehr' => 'feuerwehr',
+      'krankenhaus' => 'krankenhaus',
+      'fabrik' => 'fabrik',
+      'buro' => 'buro',
+      'rathaus' => 'rathaus',
+    }
+  end
+
+  def self.verkehr
+    {
+      '-- VERKEHR --' => '',
+      'fuss' => 'fuss',
+      'strasse' => 'strasse',
+      'auto' => 'auto',
+      'schiene' => 'schiene',
+      'schiffe' => 'schiffe',
+      'flugzeuge' => 'flugzeuge',
+      'lasten' => 'lasten',
+    }
+  end
+
+  def self.wetter
+    {
+      '-- WETTER --' => '',
+      'wetter' => 'wetter',
+      'wetterbericht' => 'wetterbericht',
+    }
+  end
+
+  def self.zeit
+    {
+      '-- ZEIT --' => '',
+      'zeit' => 'zeit',
+      'jahreszeiten' => 'jahreszeiten',
+      'geburtstag' => 'geburtstag',
+    }
+  end
+
+  def self.welt
+    {
+      '-- WELT --' => '',
+      'ausland' => 'ausland',
+      'erde' => 'erde',
+      'worterbuch' => 'worterbuch',
+    }
+  end
+
+  def self.math
+    {
+      '-- MATH --' => '',
+      'nummer' => 'nummer',
     }
   end
 
